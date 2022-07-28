2f8a49e0abb72ef3e6b8cb18b8ba8f9b964dbb8f
Owos <owodunniabraham@gmail.com>
2022-07-28 13:43:57 +0000

    added links to cnn weights

--- models/get_pretrained_weights.py
+++ models/get_pretrained_weights.py
@@ -16,6 +16,10 @@ def download_weights(model_name, url, num_class):
 if __name__ == '__main__':
     fc_195_link = 'https://drive.google.com/uc?id=1-lWUFD3wxuWEtiR1H-MQtKlJh6MF6hia'
     fc_5_link = 'https://drive.google.com/uc?id=1-3nQkgAtznQYHYOpwcxf9IUFhDX4YxmQ'
+    cnn_195 = 'https://drive.google.com/uc?id=1Dege0moAL5ZgY0l3y3udeqfTULn0XBim'
+    cnn_5 = 'https://drive.google.com/uc?id=1LvkCdmzHb7FgcsnjkxmawDf8qdTdWIhd'
 
     download_weights('fc', fc_195_link, 195)
     download_weights('fc', fc_5_link, 5)
+    download_weights('cnn_195', cnn_195, 195)
+    download_weights('cnn_5', cnn_5, 5)
